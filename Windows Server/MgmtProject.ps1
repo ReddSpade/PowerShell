61e48f4ca41608d7ccd0fae6e5e29f88795ea736
ReddSpade <matheo.rodriguez@outlook.fr>
2023-08-22 17:19:49 +0200

    Update MgmtProject.ps1

--- Windows Server/MgmtProject.ps1
+++ Windows Server/MgmtProject.ps1
@@ -1,188 +1,3 @@
-function PrAd {
-    $Title = "Menu de sélection Pré AD"
-    $Prompt = "Faire choix"
-    $IP = [System.Management.Automation.Host.ChoiceDescription]::New("Configuration des &IP","Attribution d'IP Statique ou DHCP, setup du DNS, etc..")
-    $WorkStation = [System.Management.Automation.Host.ChoiceDescription]::New("Configuration des &Postes","Modification du nom du poste, Initialisation d'un disque, création des 3 disques pour AD (Sysvol, BDD, Logs)")
-    $Domain = [System.Management.Automation.Host.ChoiceDescription]::New("Cofiguration du &Domaine","Ajout du poste en tant qu'utilisateur ou contrôleur du Domaine")
-    $Options = [System.Management.Automation.Host.ChoiceDescription[]]($IP, $WorkStation, $Domain)
-    $Choice = $host.UI.PromptForChoice($Title, $Prompt, $Options, 0)
-    Switch ($Choice) {
-        0 { $Title = "Configuration des IP"
-            $Prompt = "Faire choix"
-            $DHCP = [System.Management.Automation.Host.ChoiceDescription]::New("Mode &DHCP","Attribution d'IP par le DHCP")
-            $Static = [System.Management.Automation.Host.ChoiceDescription]::New("Mode &Statique","Attribution d'une IP en Statique")
-            $DNS = [System.Management.Automation.Host.ChoiceDescription]::New("Modification du D&NS","Modification du DNS par défaut sur lequel la machine pointe")
-            $Options = [System.Management.Automation.Host.ChoiceDescription[]]($DHCP, $Static, $DNS)
-            $Choice = $host.UI.PromptForChoice($Title, $Prompt, $Options, 0)
-            Switch ($Choice) {
-                0 { Get-NetIPConfiguration | Select-Object -Property InterfaceDescription,InterfaceIndex,IPv4AddressNetIP
-                    [int]$SelectNIC = Read-Host "Index NIC ?"
-                    Remove-NetRoute -InterfaceIndex $SelectNIC -Confirm:$false; Remove-NetIPAddress -InterfaceIndex $SelectNIC -Confirm:$false -ErrorAction SilentlyContinue
-                    Set-NetIPInterface -InterfaceIndex $SelectNIC -DHCP Enabled;console
-                }
-                1 { Get-NetIPConfiguration | Select-Object -Property InterfaceDescription,InterfaceIndex,IPv4AddressNetIP
-                    [int]$SelectNIC = Read-Host "Index NIC ?"
-                    $IPAdress = Read-Host "IP Souhaitée ?"
-                    Remove-NetRoute -InterfaceIndex $SelectNIC -Confirm:$false -ErrorAction SilentlyContinue
-                    Remove-NetIPAddress -InterfaceIndex $SelectNIC -Confirm:$false -ErrorAction SilentlyContinue
-                    $CIDR = Read-Host "Choisir le CIDR"
-                    $choix = Read-Host "Appliquer un masque sous réseau ? (Y/N)"
-                    if ($choix -eq "oui" -or $choix -eq "yes" -or $choix -eq "y") {
-                    $Mask = Read-Host "Choisir le masque sous-reseau"
-                    New-NetIPAddress -InterfaceIndex $SelectNIC -IPAddress $IPAdress -AddressFamily IPv4 -PrefixLength $CIDR -DefaultGateway $Mask;console
-                    }
-                    elseif ($choix -eq "no" -or $choix -eq "non" -or $choix -eq "n") {
-                    New-NetIPAddress -InterfaceIndex $SelectNIC -IPAddress $IPAdress -AddressFamily IPv4 -PrefixLength $CIDR;console
-                    }
-                }
-                2 { Get-NetIPConfiguration | Select-Object -Property InterfaceDescription,InterfaceIndex,IPv4AddressNetIP
-                    [int]$SelectNIC = Read-Host "Choisir le numero NIC souhaitee"
-                    $DNSIP = Read-Host "Choisir les IP souhaitees"
-                    Set-DnsClientServerAddress -InterfaceIndex $SelectNIC -Addresses $DNSIP;console
-                }
-            }
-        }
-        1 { $Title = "Configuration des postes"
-            $Prompt = "Faire choix"
-            $Rename = [System.Management.Automation.Host.ChoiceDescription]::New("&Renommer le poste","Change l'ID du poste et le redémarre pour appliquer le changement")
-            $Options = [System.Management.Automation.Host.ChoiceDescription[]]($Rename)
-            $Choice = $host.UI.PromptForChoice($Title, $Prompt, $Options, 0)
-            Switch ($Choice) {
-                0 { Write-Host "Nom actuel du poste: $env:COMPUTERNAME" -ForegroundColor DarkCyan
-                    $NewName = Read-Host -Prompt "Indiquer le nouveau nom du poste"
-                    Rename-Computer -NewName $NewName.ToUpper()
-                    Write-Warning "Le poste va maintenant redémarrer"
-                    Restart-Computer -Force
-                }
-            }
-        }
-        2 { $Title = "Configuration du Domaine"
-            $Prompt = "Faire choix"
-            $ADUser = [System.Management.Automation.Host.ChoiceDescription]::New("Rejoindre le domaine en tant qu'&Utilisateur","Permet d'ajouter le poste de travail dans le domaine en tant qu'Ordinateur Standard")
-            $ADDC = [System.Management.Automation.Host.ChoiceDescription]::New("Rejoindre le domaine en tant que &Contrôleur de Domaine","Permet d'ajouter le poste de travail dans le domaine en tant que Contrôleur de Domaine numéro 2")
-            $ADMainDC = [System.Management.Automation.Host.ChoiceDescription]::New("Créer la Forêt &Active Directory","Création de la Forêt Active Directory, promotion en tant que Contrôleur de Domaine + Configuration DNS")
-            $Options = [System.Management.Automation.Host.ChoiceDescription[]]($ADUser, $ADDC, $ADMainDC)
-            $Choice = $host.UI.PromptForChoice($Title, $Prompt, $Options, 0)
-            Switch ($Choice) {
-                0 { $DNSIP = Get-DnsClientServerAddress -AddressFamily IPv4 | Select-Object -ExpandProperty ServerAddresses | Select-Object -First 1
-                    $FQDN = (Resolve-DnsName -Name $DNSIP).NameHost
-                    $DomainRaw = $FQDN -Split "\." | Select-Object -Last 2
-                    $DomainName = $DomainRaw -Join "."
-                    $DomainNETBIOSRaw = $DomainRaw | Select-Object -SkipLast 1
-                    $DomainNETIBIOS = "$DomainNETBIOSRaw\".ToUpper()
-                    $DomainAdmin = "$($DomainNETIBIOS)Administrateur"
-                    Add-Computer -Domain $DomainName -Restart -Credential $DomainAdmin
-                }
-                1 { Import-Module -Name PSScheduledJob
-                    Register-ScheduledJob -Name "ReverseDNSSetup" -ScriptBlock {
-                    $NIC = (Get-NetAdapter).ifIndex
-                    $DomainMainDC = (Get-ADDomain).DNSRoot
-                    $DomainMainDCIP = (Resolve-DNSName -Name $DomainMainDC | Where-Object -Property Type -eq A).IPAddress
-                    Get-DNSClientServerAddress -InterfaceIndex $NIC -AddressFamily IPv6,IPv4 | Set-DnsClientserveraddress -ResetServerAddresses
-                    Set-DnsClientServerAddress -InterfaceIndex $NIC -ServerAddresses $DomainMainDCIP
-                    ipconfig /registerdns
-                    Unregister-ScheduledJob *
-                    Remove-Job *
-                    }
-                    $Disk = Get-Disk -Number 3 -ErrorAction SilentlyContinue
-                    if ($Null -eq $Disk) {
-                        Write-Host "Disques SYSVOL BDD & LOGS absents de la VM"
-                    }
-                    else {
-                    Initialize-Disk -Number 1
-                    New-Partition -DiskNumber 1 -DriveLetter B -UseMaximumSize
-                    Format-Volume -DriveLetter B -FileSystem NTFS -Confirm:$false -NewFileSystemLabel BDD
-                    Initialize-Disk -Number 2
-                    New-Partition -DiskNumber 2 -DriveLetter L -UseMaximumSize
-                    Format-Volume -DriveLetter L -FileSystem NTFS -Confirm:$false -NewFileSystemLabel LOGS
-                    Initialize-Disk -Number 3
-                    New-Partition -DiskNumber 3 -DriveLetter S -UseMaximumSize
-                    Format-Volume -DriveLetter S -FileSystem NTFS -Confirm:$false -NewFileSystemLabel SYSVOL
-
-                    Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
-                    Import-Module ADDSDeployment
-                    $DNSIP = Get-DnsClientServerAddress | Select-Object -ExpandProperty ServerAddresses | Select-Object -First 1
-                    $FQDN = (Resolve-DnsName -Name $DNSIP).NameHost
-                    $DomainRaw = $FQDN -Split "\." | Select-Object -Last 2
-                    $DomainName = $DomainRaw -Join "."
-                    $DomainNETBIOSRaw = $DomainRaw | Select-Object -SkipLast 1
-                    $DomainNETIBIOS = "$DomainNETBIOSRaw".ToUpper()
-                    $DomainAdmin = "$DomainNETIBIOS\Administrateur"
-                    $ForestConfiguration = @{
-                        '-DatabasePath'           = 'B:\NTDS';
-                        '-DomainName'             = $DomainName;
-                        '-InstallDns'             = $true;
-                        '-LogPath'                = 'L:\NTDS';
-                        '-NoRebootOnCompletion'   = $false;
-                        '-SysvolPath'             = 'S:\SYSVOL';
-                        '-Force'                  = $true;
-                        '-CreateDnsDelegation'    = $false;
-                        '-NoGlobalCatalog'        = $false;
-                        '-ReplicationSourceDC'    = $FQDN;
-                        '-CriticalReplicationOnly'= $false;
-                        '-SiteName'               = "Default-First-Site-Name";
-                        '-Credential'             =  (Get-Credential $DomainAdmin)
-                        }
-                        if ($DomainNETIBIOS.Length -le 1 ) {
-                            Write-Error "Erreur DNS, regarder le Contrôleur Principal du Domaine..." -ErrorAction Break
-                        }
-                        else {
-                        Install-ADDSDomainController @ForestConfiguration
-                        }
-                    }
-                }
-                2 { Import-Module -Name PSScheduledJob
-                    Register-ScheduledJob -Name "ReverseDNSSetup" -ScriptBlock {
-                    $NIC = (Get-NetAdapter).ifIndex
-                    $FQDNDC01IP = (Get-NetIPAddress -InterfaceIndex $NIC -AddressFamily IPv4).IPAddress
-                    Get-DNSClientServerAddress -InterfaceIndex $NIC -AddressFamily IPv6,IPv4 | Set-DnsClientserveraddress -ResetServerAddresses
-                    Set-DnsClientServerAddress -InterfaceIndex $NIC -ServerAddresses $FQDNDC01IP
-                    $NetworkIP = $FQDNDC01IP -Split "\."; $NetworkIP[3] = 0; $NetworkIP = $NetworkIP -Join "."; $NetworkIP += "/24"
-                    Add-DNSServerPrimaryZone -NetworkId $NetworkIP -ReplicationScope Domain -DynamicUpdate Secure
-                    ipconfig /registerdns
-                    Unregister-ScheduledJob *
-                    Remove-Job *
-                    } -Trigger (New-JobTrigger -AtStartup) -ScheduledJobOption (New-ScheduledJobOption -RunElevated)
-
-                    $Disk = Get-Disk -Number 3 -ErrorAction SilentlyContinue
-                    if ($Null -eq $Disk) {
-                        Write-Host "Disques SYSVOL BDD & LOGS absents de la VM"
-                    }
-                    else {
-                    Initialize-Disk -Number 1
-                    New-Partition -DiskNumber 1 -DriveLetter B -UseMaximumSize
-                    Format-Volume -DriveLetter B -FileSystem NTFS -Confirm:$false -NewFileSystemLabel BDD
-                    Initialize-Disk -Number 2
-                    New-Partition -DiskNumber 2 -DriveLetter L -UseMaximumSize
-                    Format-Volume -DriveLetter L -FileSystem NTFS -Confirm:$false -NewFileSystemLabel LOGS
-                    Initialize-Disk -Number 3
-                    New-Partition -DiskNumber 3 -DriveLetter S -UseMaximumSize
-                    Format-Volume -DriveLetter S -FileSystem NTFS -Confirm:$false -NewFileSystemLabel SYSVOL
-
-                    $NameDomain = Read-Host "Nommez le domaine"
-                    $NameNetBIOS = $NameDomain -Split "\."; $NameNetBIOS = $NameNetBIOS[0].ToUpper()
-                    Add-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools -IncludeAllSubFeature
-                    Import-Module ADDSDeployment
-                    $NewForestConfiguration = @{
-                        '-CreateDnsDelegation'     = $false;
-                        '-DatabasePath'            = 'B:\NTDS';
-                        '-DomainMode'              = 'WinThreshold';
-                        '-DomainName'              = $NameDomain;
-                        '-DomainNetbiosName'       = $NameNetBIOS;
-                        '-ForestMode'              = 'WinThreshold';
-                        '-InstallDns'              = $true;
-                        '-LogPath'                 = 'L:\NTDS';
-                        '-NoRebootOnCompletion'    = $false;
-                        '-SysvolPath'              = 'S:\SYSVOL';
-                        '-Force'                   = $true;
-                    }
-                    Install-ADDSForest @NewForestConfiguration
-                    }
-                }
-            }
-        }
-    }
-}
 function PostAd {
     $Title = "Menu de sélection Post AD"
     $Prompt = "Faire choix"
@@ -439,8 +254,8 @@ function PostAd {
             }
         }
         2 { $DomainRootOU = "$(Get-ADDomain)"
-            $RootLayerA = "Direction"
-            $RootLayerB = "Service"
+            $RootLayerA = "Service"
+            $RootLayerB = "Direction"
             $RootLayerC = "Commun"
             $GeneralOU = @("Utilisateurs","Groupes","Ordinateurs","Imprimantes")
             $Title = "Configuration Active Directory"
@@ -475,8 +290,25 @@ function PostAd {
                         New-ADOrganizationalUnit -Name $_ -Path "OU=$RootLayerC,OU=$RootLayer1,$DomainRootOU" -ProtectedFromAccidentalDeletion:$false
                         }
                 }
-                1 { Write-Host "Work-In-Progress !" -ForegroundColor Green; Start-Sleep -Seconds 1; console }
-                2 { $Script:Layers = @($RootLayerA, $RootLayerB, $RootLayerC)
+                1 { $Layers = @($RootLayerA, $RootLayerB, $RootLayerC)
+                       $Layers | Foreach-Object {
+
+                        $GlobalGroup = "GG_$_"
+                        $UniversalGroup = "GU_$_"
+                        $DomainLocalGroup = "GDL_$($_)_RW"
+                        $Groups = @($GlobalGroup,$UniversalGroup,$DomainLocalGroup)
+
+                        $Path = "OU=$($GeneralOU[1]),OU=$_,OU=$(Get-ADOrganizationalUnit -Filter * -SearchBase $(Get-ADDomain) -SearchScope OneLevel | Where-Object -Property Name -NotLike *Domain* | Select-Object -ExpandProperty Name),$DomainRootOU"
+
+                        $Groups | Foreach-Object {
+                            New-ADGroup -GroupCategory Security -GroupScope Global -Name $_ -Path $Path -Verbose
+
+                            Add-ADGroupMember -Identity $UniversalGroup -Members  $GlobalGroup -Verbose
+                            Add-ADGroupMember -Identity $DomainLocalGroup -Members  $UniversalGroup -Verbose
+                        }
+                    }
+                }
+                2 { $Layers = @($RootLayerA, $RootLayerB, $RootLayerC)
                     Import-Module -Name NameIT
                     $Layers | ForEach-Object {
                         $LayerName = $_
@@ -492,23 +324,20 @@ function PostAd {
                         }
                         $CSVData | Export-CSV -Path "$env:USERPROFILE\Documents\ADUser$LayerName.csv" -Delimiter ";" -Encoding utf8 -NoTypeInformation
                     }
-                    $i=0
                     Get-ChildItem -Path "$env:USERPROFILE\Documents" | Where-Object -Property Extension -eq ".csv" | Select-Object -ExpandProperty FullName | Foreach-Object {
                         Import-Csv -Path $_ -Delimiter ";" -Encoding utf8 | Foreach-Object {
-                            $Layers = @($RootLayerA, $RootLayerB, $RootLayerC)
                             $UserSurname = $_.Surname
                             $UserGivenName = $_.GivenName
                             $UserDisplayName = "$($_.Surname.ToUpper()) $($_.GivenName)"
-                            $UserCommonName = $_.GivenName.Substring(0,1).ToLower() + $_.Surname.Substring(0).ToLower()
-                            $SamAccountName = $UserCommonName
+                            $UserCommonName = "$($_.Surname.ToUpper()) $($_.GivenName)"
+                            $SamAccountName = $_.GivenName.Substring(0,1).ToLower() + $_.Surname.Substring(0).ToLower()
                             $UserPrincipalName = "$($_.GivenName.ToLower()).$($_.Surname.ToLower())@$((Get-ADDomain).DNSRoot)"
                             $Mail = $UserPrincipalName
                             $AccountPassword = ConvertTo-SecureString "Lapinou33+" -AsPlainText -Force
-                            $Path = "OU=$($GeneralOU[0]),OU=$($Layers[$i]),OU=$(Get-ADOrganizationalUnit -Filter * -SearchBase $(Get-ADDomain) -SearchScope OneLevel | Where-Object -Property Name -NotLike *Domain* | Select-Object -ExpandProperty Name),$DomainRootOU"
-                            Wait-Debugger
+                            $Path = "OU=$($GeneralOU[0]),OU=$($_.OU),OU=$(Get-ADOrganizationalUnit -Filter * -SearchBase $(Get-ADDomain) -SearchScope OneLevel | Where-Object -Property Name -NotLike *Domain* | Select-Object -ExpandProperty Name),$DomainRootOU"
+
                             New-ADUser -Name $UserCommonName -Surname $UserSurname -GivenName $UserGivenName -SamAccountName $SamAccountName -UserPrincipalName $UserPrincipalName -DisplayName $UserDisplayName -EmailAddress $Mail -AccountPassword $AccountPassword -Path $Path  -ChangePasswordAtLogon:$true -Enabled:$true
                         }
-                        $i++
                     }
                 }
             }
